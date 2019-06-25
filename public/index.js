568c403476db3b4852e233edd07c4a60d276a4d9
awr417h <abdullahwaqar8@gmail.com>
2019-06-26 01:14:01 +0500

    LICENSE

--- public/index.js
+++ public/index.js
@@ -1,7 +1,27 @@
-new VTTCue({
+new Vue({
     el: '#app',
     data: {
         name: '',
         url: ''
+    },
+    methods: {
+        async createShorty() {
+            const API_URI = '/api/shorty';
+            const body = {
+                name: this.name,
+                url: this.url
+            };
+
+            const response = await fetch(API_URI, {
+                method: 'POST',
+                body: JSON.stringify(body),
+                headers: {
+                    'content-type': 'application/json'
+                }
+            });
+            const result = await response;
+            console.log(result);
+            return response.json();
+        }
     }
-})
+});
