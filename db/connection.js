6dd864556d0677e426b2e0612dbc9d794349c8ce
awr417h <abdullahwaqar8@gmail.com>
2019-06-26 05:37:05 +0500

    release 1.0.0

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,15 @@
 const monk = require('monk');
-const connectionURI = require('../config/keys').mongoURI;
+const nconf = require('nconf');
+
+nconf.argv().env().file('config/keys.json');
+
+const user = nconf.get('mongoUser');
+const pass = nconf.get('mongoPass');
+const host = nconf.get('mongoHost');
+const port = nconf.get('mongoPort');
+const dbName = nconf.get('mongoDatabase');
+
+let connectionURI = `mongodb://${user}:${pass}@${host}:${port}/${dbName}`;
 
 const db = monk(connectionURI);
 
